aef1ea2529ad2bbfaf76f3688046686e49486d17
jeias <jeias.soares@gmail.com>
2017-11-08 15:16:19 -0200

    initial commit

---
+++ src/main/java/com/example/jenkins/pipeline/controller/HealthController.java
@@ -0,0 +1,11 @@
+package com.example.jenkins.pipeline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public void healthCheck(){}
+}
